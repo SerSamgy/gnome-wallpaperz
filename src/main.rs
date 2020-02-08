7464b47bde79f662a6f47e3c003b51161bc9e81f
sersamgy <samgy7cool@gmail.com>
2020-02-08 18:44:30 +0300

    Make render of index.xml template with starttime section

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tera::{ Tera, Context };
+
+fn main() -> Result<(), Box<dyn ::std::error::Error>> {
+    let tera = match Tera::new("templates/**/*.xml") {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let context = Context::new();
+    let result = tera.render("index.xml", &context)?;
+    println!("{}", result);
+
+    Ok(())
 }
