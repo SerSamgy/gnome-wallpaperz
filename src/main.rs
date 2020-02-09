4ad02efd62ef840385d4a6aeae6c8a8ca1452341
sersamgy <samgy7cool@gmail.com>
2020-02-09 19:32:04 +0300

    Add lazy evaluation of templates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+#[macro_use]
+extern crate lazy_static;
+
 use tera::{ Tera, Context };
 
-fn main() -> Result<(), Box<dyn ::std::error::Error>> {
-    let tera = match Tera::new("templates/**/*.xml") {
-        Ok(t) => t,
-        Err(e) => {
-            eprintln!("Parsing error(s): {}", e);
-            ::std::process::exit(1);
-        }
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*.xml") {
+            Ok(t) => t,
+            Err(e) => {
+                eprintln!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![]);
+        tera
     };
+}
+
+fn main() -> Result<(), Box<dyn ::std::error::Error>> {
     let context = Context::new();
-    let result = tera.render("index.xml", &context)?;
+    let result = TEMPLATES.render("index.xml", &context)?;
     println!("{}", result);
 
     Ok(())
