492f5ecb4f8ef292ecd8c4040bc94a2920f95e8f
sersamgy <samgy7cool@gmail.com>
2020-02-18 23:31:52 +0300

    Add input config

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,52 @@ use chrono::prelude::*;
 use serde::Serialize;
 use tera::{Context, Tera};
 
+#[derive(Debug)]
+pub struct Config {
+  pub source_path: String,
+  pub output_filename: String,
+  pub starttime: DateTime<Local>,
+  pub duration: f64,
+  pub trans_duration: f64,
+}
+
+impl Config {
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    let source_path = match args.next() {
+      Some(sp) => sp,
+      None => return Err("Didn't get path to source folder"),
+    };
+
+    let output_filename = match args.next() {
+      Some(out) => out,
+      None => return Err("Didn't get the name of output file"),
+    };
+
+    // Optional parameters for now
+    let starttime = match args.next() {
+      Some(st) => match Local.datetime_from_str(&st, "%Y-%m-%d %H:%M:%S").ok() {
+        Some(dt) => dt,
+        None => return Err("Failed to parse provided start time"),
+      },
+      None => Local::now(), 
+    };
+
+    let default_duration = 300.0;
+    let duration = match args.next() {
+      Some(dr) => dr.parse::<f64>().unwrap_or(default_duration),
+      None => default_duration, 
+    };
+    
+    let default_trans_duration = 60.0;
+    let trans_duration = match args.next() {
+      Some(td) => td.parse::<f64>().unwrap_or(default_trans_duration),
+      None => default_trans_duration, 
+    };
+
+    Ok(Config{ source_path, output_filename, starttime, duration, trans_duration })
+  }
+}
+
 #[derive(Serialize, Debug)]
 pub struct IndexContext {
     pub starttime: DateTime<Local>,
@@ -33,13 +79,7 @@ impl IndexContext {
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/**/*.xml") {
-            Ok(t) => t,
-            Err(e) => {
-                eprintln!("Parsing error(s): {}", e);
-                ::std::process::exit(1);
-            }
-        };
+        let mut tera = Tera::new("templates/**/*.xml").unwrap();
         tera.autoescape_on(vec![]);
         tera.register_filter("with_zero", filters::with_zero);
         tera
